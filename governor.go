1abcaad7472d964a7a0a7596d28a46ccc0bfcc4a
Jonny Elliott <jonnynelliott@googlemail.com>
2015-07-31 14:28:30 -0400

    Updating formatting

--- governor.go
+++ governor.go
@@ -2,87 +2,87 @@
 package main
 
 import (
-	"os"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"log"
-	"net/http"
 	"github.com/hashicorp/consul/api"
 	"io/ioutil"
-	"encoding/json"
+	"log"
+	"net/http"
+	"os"
 )
 
 const (
 	CONSUL_ADDRESS string = "CONSUL_HOST"
-	CONSUL_PORT string = "CONSUL_PORT"
+	CONSUL_PORT    string = "CONSUL_PORT"
 )
 
-func GetAttribute (key string, defaultClient *http.Client) string {
+func GetAttribute(key string, defaultClient *http.Client) string {
 
 	// Get client
 	config := api.DefaultConfig()
-	
+
 	// Override the client if we want more control
 	if defaultClient != nil {
-		config.HttpClient = defaultClient	
+		config.HttpClient = defaultClient
 	}
-	
+
 	// Check environment variables
 	consulPort := os.Getenv(CONSUL_PORT)
 	if consulPort == "" {
 		consulPort = "8500"
 	}
-	
+
 	if consulAddress := os.Getenv(CONSUL_ADDRESS); consulAddress != "" {
 		config.Address = consulAddress + ":" + consulPort
 	}
 	log.Println("Set the address to: ", config.Address)
-	
+
 	// Load the client
 	client, _ := api.NewClient(config)
-	
+
 	// Key-value end point
 	kv := client.KV()
-	
-        log.Println("Attempting to retrieve key: ", key)
+
+	log.Println("Attempting to retrieve key: ", key)
 	keyValue, _, err := kv.Get(key, nil)
 	if err != nil {
-                log.Fatal("Error raised when attempting to get keys from consul: ", err)
+		log.Fatal("Error raised when attempting to get keys from consul: ", err)
 	}
-        if keyValue == nil {
-                log.Fatal("Key supplied returned a nil value - does it exist: ", keyValue)
-        }
+	if keyValue == nil {
+		log.Fatal("Key supplied returned a nil value - does it exist: ", keyValue)
+	}
 
 	// Get the value and convert to string
 	byteValue := keyValue.Value
-	
+
 	stringValue := string(byteValue[:])
-        log.Println("Consul returned", stringValue)
+	log.Println("Consul returned", stringValue)
 
 	return stringValue
 }
 
-func GetConfigFromFile (fileName string) map[string]string {
-	
+func GetConfigFromFile(fileName string) map[string]string {
+
 	// Open the file
 	contents, err := ioutil.ReadFile(fileName)
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Unmarshal the JSON content
 	configMap := make(map[string]string)
 	json.Unmarshal(contents, &configMap)
-	
+
 	return configMap
 }
 
-func MakeConfigFiles (configMap map[string]string) {
-	
+func MakeConfigFiles(configMap map[string]string) {
+
 	for filePath, fileContents := range configMap {
 		// Write the file with its relevant contents
-                log.Printf("Writing config file %s\n", filePath)
-		ioutil.WriteFile(filePath, []byte(fileContents), 0644)		
+		log.Printf("Writing config file %s\n", filePath)
+		ioutil.WriteFile(filePath, []byte(fileContents), 0644)
 	}
 
 }
@@ -92,40 +92,40 @@ func checkFileExists(fileName string) {
 	if err != nil {
 		fmt.Println("You need to specify a config file that exists.")
 		panic(err)
-	}	
+	}
 }
 
-func Govern (configFile string, defaultClient *http.Client) {
-	
+func Govern(configFile string, defaultClient *http.Client) {
+
 	// Parse the config file
 	configMap := GetConfigFromFile(configFile)
-	
+
 	// Obtain the content from Consul and place in map
 	var configContent string
 	outputConfigMap := make(map[string]string)
-	
+
 	for consulKey, configPath := range configMap {
 		configContent = GetAttribute(consulKey, defaultClient)
-		
+
 		outputConfigMap[configPath] = configContent
 	}
-	
+
 	// Make the config files
 	MakeConfigFiles(outputConfigMap)
 }
 
-func main () {
-	
+func main() {
+
 	// Definitions of allowed input flags
 	configFilePtr := flag.String("c", "govern.conf", "Config file.")
-	
+
 	// Parse all the flags based on definitions
 	flag.Parse()
-	
+
 	// Check config file exists
 	checkFileExists(*configFilePtr)
-	
+
 	// Runtime routine
-        log.Println("Using config file: ", *configFilePtr)
+	log.Println("Using config file: ", *configFilePtr)
 	Govern(*configFilePtr, nil)
 }
--- governor_test.go
+++ governor_test.go
@@ -2,15 +2,15 @@
 package main
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"	
-	"testing"
-	"net/http"
-	"net/url"
-	"net/http/httptest"
 	"encoding/base64"
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
 	"os"
+	"testing"
 )
 
 type StubConfig struct {
@@ -18,9 +18,9 @@ type StubConfig struct {
 }
 
 func TestConsulAccess(t *testing.T) {
-	
+
 	expected := StubConfig{key: "ssl_key", value: "/path/to/key"}
-	
+
 	// Test server that always responds with 200 code, and specific payload
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
@@ -38,7 +38,7 @@ func TestConsulAccess(t *testing.T) {
 
 		fmt.Fprintln(w, response)
 	}))
-	
+
 	defer server.Close()
 
 	// Make a transport that reroutes all traffic to the example server
@@ -52,13 +52,13 @@ func TestConsulAccess(t *testing.T) {
 	httpClient := &http.Client{Transport: transport}
 
 	attr := GetAttribute(expected.key, httpClient)
-	
+
 	assert.Equal(t, attr, expected.value, "The two words should be equal")
-	
+
 }
 
 func TestParseFileCorrectly(t *testing.T) {
-	
+
 	expected := StubConfig{key: "ssl_key", value: "/path/to/key"}
 
 	// Make a stub file
@@ -68,31 +68,31 @@ func TestParseFileCorrectly(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Cleanup the file that was made
 	defer os.Remove(stubFileName)
-	
+
 	// Load the config file
 	config := GetConfigFromFile(stubFileName)
-	
+
 	// Check that the key exists
-	value, ok := config[expected.key]	
+	value, ok := config[expected.key]
 	assert.True(t, ok)
 	assert.Equal(t, value, expected.value)
-	
+
 }
 
 func TestConsulWriteToDisk(t *testing.T) {
-	
+
 	expected_file := "config.conf"
-	
+
 	stubMap := map[string]string{
 		expected_file: `{"ssl_key": "/path/to/key"}`,
 	}
 
 	// Generate a file from the stub data
 	MakeConfigFiles(stubMap)
-	
+
 	// Check that the config file was created
 	_, err := os.Stat(expected_file)
 	assert.Nil(t, err)
@@ -105,7 +105,7 @@ func TestConsulWriteToDisk(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
-	
+
 	// Check the file contains relevant information
 	assert.Contains(
 		t,
@@ -113,16 +113,15 @@ func TestConsulWriteToDisk(t *testing.T) {
 		"ssl_key",
 		"The output should contain specific text, but it does not",
 	)
-	
+
 }
 
 func TestGovern(t *testing.T) {
-	
-	
+
 	// Start the test server that intercepts our requests
 	stubConsulResponse := StubConfig{key: "ssl_key", value: `{"ouput": "disk"}`}
 	stubGovernorConfig := StubConfig{key: "ssl_key", value: "lsf.conf"}
-	
+
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
 		valueBase64 := base64.StdEncoding.EncodeToString([]byte(stubConsulResponse.value))
@@ -139,7 +138,7 @@ func TestGovern(t *testing.T) {
 
 		fmt.Fprintln(w, response)
 	}))
-	
+
 	defer server.Close()
 
 	// Make a transport that reroutes all traffic to the example server
@@ -150,19 +149,19 @@ func TestGovern(t *testing.T) {
 	}
 
 	// Make a http.Client with the transport
-	httpClient := &http.Client{Transport: transport}	
-	
+	httpClient := &http.Client{Transport: transport}
+
 	// Make a stub config file
 	stubConfig := "governor.conf"
-	stubContent := fmt.Sprintf(`{"%s": "%s"}`, 
-							stubGovernorConfig.key,
-							stubGovernorConfig.value)
+	stubContent := fmt.Sprintf(`{"%s": "%s"}`,
+		stubGovernorConfig.key,
+		stubGovernorConfig.value)
 	err := ioutil.WriteFile(stubConfig, []byte(stubContent), 0644)
 	if err != nil {
 		panic(err)
 	}
 	defer os.Remove(stubConfig)
-	
+
 	// Lets run the routine
 	Govern(stubConfig, httpClient)
 
@@ -170,14 +169,14 @@ func TestGovern(t *testing.T) {
 	_, err = os.Stat(stubGovernorConfig.value)
 	assert.Nil(t, err)
 	defer os.Remove(stubGovernorConfig.value)
-	
+
 	// Check the contents is sensible
 	var contents []byte
 	contents, err = ioutil.ReadFile(stubGovernorConfig.value)
 	if err != nil {
 		panic(err)
 	}
-		
+
 	// Check the file contains relevant information
 	assert.Contains(
 		t,
